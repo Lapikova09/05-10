d0a1133d0f30d8389a56e0e8ef8bf5a5dfc353ef
Lapikova09 <96071466+Lapikova09@users.noreply.github.com>
2022-10-05 17:55:58 +0300

    Add files via upload

---
+++ Source.cpp17.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "russian");
+	//задача 1
+	//int hp;
+	//cin >> hp;
+	//bool res = hp > 0;
+	//cout << boolalpha << res << endl;
+	//system("pause");
+
+	//задача 2
+	//int t;
+	//cin >> t;
+	//if (t > 20) { cout << "on" << endl; }
+	//else{ cout << "off" << endl; }
+	//system("pause");
+
+	//задача 3
+	//int T, t, v;
+	//cin >> T >> t >> v;
+	//if ( t > T && v<=80) { cout << "on" << endl; }
+	//else { cout << "off" << endl; }
+	//system("pause");
+
+	//задача 4
+	//double y,x;
+	//cin >> x;
+	//if (x > 0) { y = sqrt(x - 2); cout << y << endl; }
+	//else if (x == 0) { y = x*0; cout << y << endl; }
+	//else if (x < 0) { y = abs(x); cout << y << endl;}
+    //system("pause");
+
+	//задача 5
+	//int number;
+	//cin >> number;
+	//switch (number) {
+	//case 1:cout << "большой" << endl; break;
+	//case 2:cout << "указательный" << endl; break;
+	//case 3:cout << "средний" << endl; break;
+	//case 4:cout << "безымянный" << endl; break;
+	//case 5:cout << "мизинец" << endl; break;
+	//default:cout << "ошибка" << endl;
+	//}
+	//system("pause");
+
+
+	//задача 6
+	//int t, T;
+	//cin >> t;
+	//cin >> T;
+	//if (t > 20 && T > t) { cout << "ac on" << endl; }
+	//else if (t <= 20 && T > t) { cout << "ac off" << endl; }
+	//else if  (t > T) { cout << "open window" << endl; }
+	//system("pause");
+
+
+
+}
